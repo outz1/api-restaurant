6091928699218b4b50f2ae63d03e06ad8ecba93b
Eduardo de Brito Soares <du.ebrits@gmail.com>
2025-04-29 12:10:41 -0300

    sum of orders and other ajusts

--- src/controllers/orders-controller.ts
+++ src/controllers/orders-controller.ts
@@ -2,9 +2,11 @@ import { Request, Response, NextFunction } from "express";
 import { z } from "zod";
 import { knex } from "@/database/knex";
 import { AppError } from "@/utils/AppError";
+import { notDeepEqual } from "assert";
 
 class OrdersController {
-  async create(request: Request, response: Response, next: NextFunction) { // POST
+  async create(request: Request, response: Response, next: NextFunction) {
+    // POST
     try {
       const bodySchema = z.object({
         table_session_id: z.number(),
@@ -35,7 +37,6 @@ class OrdersController {
       if (!product) {
         throw new AppError("Product not found", 404);
       }
-      
 
       await knex<OrderRepository>("orders").insert({
         table_session_id,
@@ -50,13 +51,48 @@ class OrdersController {
     }
   }
 
-  async index(request: Request, response: Response, next: NextFunction)  { // GET
+  async index(request: Request, response: Response, next: NextFunction) {
+    // GET
     try {
+      const { table_session_id } = request.params;
 
-      
-      return response.json()
+      const orders = await knex("orders")
+        .select(
+          "orders.id",
+          "orders.table_session_id",
+          "orders.product_id",
+          "products.name",
+          "orders.price",
+          "orders.quantity",
+          knex.raw("(orders.price * orders.quantity) as total_R$"), // calculo do qntd * valor
+          "orders.created_at",
+          "orders.updated_at"
+        ) // tabela + coluna
+        .join("products", "products.id", "orders.product_id") // conectando com a tabela products
+        .where({ table_session_id })
+        .orderBy("orders.created_at", "desc");
+
+      return response.json(orders);
     } catch (error) {
-      next(error)
+      next(error);
+    }
+  }
+
+  async show(request: Request, response: Response, next: NextFunction) {
+    try {
+      const { table_session_id } = request.params;
+
+      const order = await knex("orders")
+        .select(knex.raw("COALESCE(SUM(orders.price * orders.quantity), 0) AS total_R$"))
+        .select(knex.raw("COALESCE(SUM(orders.quantity), 0) AS total_quantity"))
+        .where({ table_session_id })
+        .first();
+
+        // arredondar casa decimal
+        order.total_R$ = Number(Number(order.total_R$).toFixed(2))
+      return response.json(order);
+    } catch (error) {
+      next(error);
     }
   }
 }
--- src/routes/orders-routes.ts
+++ src/routes/orders-routes.ts
@@ -6,6 +6,7 @@ const ordersRoutes = Router();
 const ordersController = new OrdersController();
 
 ordersRoutes.post("/", ordersController.create); // Define a rota POST para criar os produtos (método create)
-ordersRoutes.get("/table-session/:id", ordersController.index); // Define a rota GET para listar os produtos (método index)
+ordersRoutes.get("/table-session/:table_session_id", ordersController.index); // Define a rota GET para listar os produtos (método index)
+ordersRoutes.get("/table-session/:table_session_id/total", ordersController.show) // Define a rota GET para somar os produtos (método show)
 
 export { ordersRoutes };
